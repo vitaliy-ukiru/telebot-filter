31d918ca0d686a6dc1d6a8b3f6b0bfd066e18eea
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-03-23 23:52:13 +0500

    fix(text): edit panic's prefix to telebot-filter

--- internal/endpoint.go
+++ internal/endpoint.go
@@ -9,6 +9,6 @@ func ExtractRawEndpoint(e any) string {
 	case tb.CallbackEndpoint:
 		return end.CallbackUnique()
 	default:
-		panic("fsm: telebot: unsupported endpoint")
+		panic("telebot-filter: unsupported endpoint")
 	}
 }
