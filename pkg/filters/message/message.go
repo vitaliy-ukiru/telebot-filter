e168c97a355bb305ee67b221b8b9bd44352de6dd
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-04-29 00:00:38 +0500

    docs(filters/message): add Regexp filter and entity's filters

--- pkg/filters/message/message.go
+++ pkg/filters/message/message.go
@@ -2,6 +2,7 @@
 package message
 
 import (
+	"regexp"
 	"strings"
 
 	tf "github.com/vitaliy-ukiru/telebot-filter/telefilter"
@@ -18,4 +19,33 @@ func EqualFold(s string) tf.Filter {
 	return func(c tb.Context) bool {
 		return strings.EqualFold(c.Text(), s)
 	}
+}
+
+func Regexp(pattern regexp.Regexp) tf.Filter {
+	return func(c tb.Context) bool {
+		return pattern.MatchString(c.Text())
+	}
+}
+
+// EntityFunc checks message's entities at returns true if
+// at least one matches predicate.
+func EntityFunc(predicate func(entity tb.MessageEntity) bool) tf.Filter {
+	return func(c tb.Context) bool {
+		for _, entity := range c.Entities() {
+			if predicate(entity) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+func HaveEntities(c tb.Context) bool {
+	return len(c.Entities()) > 0
+}
+
+func HaveEntity(entityType tb.EntityType) tf.Filter {
+	return EntityFunc(func(entity tb.MessageEntity) bool {
+		return entity.Type == entityType
+	})
 }
