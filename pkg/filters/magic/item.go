61e413b31bcef84a89cfef2f66f9bc25cf2aaf81
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-05-19 13:13:08 +0500

    chore(magic): move some utils to item.go

--- pkg/filters/magic/item.go
+++ pkg/filters/magic/item.go
@@ -50,3 +50,58 @@ func (c CompareFilter[T]) OneOf(values ...T) tf.Filter {
 func (c CompareFilter[T]) On(filter ItemFilter[T]) tf.Filter {
 	return newPredicate(c.getter, filter)
 }
+
+func (c CompareFilter[T]) All(factories ...FilterFactory[CompareFilter[T]]) tf.Filter {
+	return logicBranch(c, And, factories)
+}
+
+func (c CompareFilter[T]) Any(factories ...FilterFactory[CompareFilter[T]]) tf.Filter {
+	return logicBranch(c, Or, factories)
+}
+
+type FilterFactory[T any] func(T) tf.Filter
+
+func logicBranch[T any](
+	base T,
+	operator func(...tf.Filter) tf.Filter,
+	factories []FilterFactory[T],
+) tf.Filter {
+	filters := make([]tf.Filter, 0, len(factories))
+	for _, factory := range factories {
+		filters = append(filters, factory(base))
+	}
+	return operator(filters...)
+}
+
+func newBoolFilter(getter ItemGetter[bool], want bool) tf.Filter {
+	return newPredicate(getter, boolFilter(want))
+}
+
+func joinGetter[T, U any](a ItemGetter[T], b func(T) U) ItemGetter[U] {
+	return func(ctx tele.Context) (U, bool) {
+		t, ok := a(ctx)
+		if !ok {
+			var zero U
+			return zero, false
+		}
+		return b(t), true
+	}
+}
+
+func joinNotNil[T any, U *K, K any](a ItemGetter[T], b func(T) U) ItemGetter[U] {
+	return func(ctx tele.Context) (U, bool) {
+		t, ok := a(ctx)
+		if !ok {
+			var zero U
+			return zero, false
+		}
+		v := b(t)
+		return v, v != nil
+	}
+}
+
+func boolFilter(want bool) ItemFilter[bool] {
+	return func(got bool) bool {
+		return got == want
+	}
+}
